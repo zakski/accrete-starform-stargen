6e9389030ee2f0bde866ce4c3403fab5b02961d9
zakski <szadowsz@tcd.ie>
2015-07-13 11:30:21 +0100

    Finished Comments for Base Accretion Entities

--- src/main/scala/com/szadowsz/accrete/base/bodies/DustBand.scala
+++ src/main/scala/com/szadowsz/accrete/base/bodies/DustBand.scala
@@ -1,7 +1,8 @@
 package com.szadowsz.accrete.base.bodies
 
+import java.text.DecimalFormat
+
 /**
- *
  * Class to represent a band of dust of gas. Contains the inner and outer edge, and whether it has dust or
  * gas present. Uses an internal linked-list structure to maintain the list of bands.
  *
@@ -9,34 +10,83 @@ package com.szadowsz.accrete.base.bodies
  * @see struct dust_record, line 37 in structs.h - Mat Burdick (accrete)
  * @see struct dust_record, line 37 in structs.h - Mat Burdick (starform)
  * @see DustBand.java - Carl Burke (starform)
-
  *
- * @param innerLimit inner limit of the dust band
- * @param outerLimit outer limit of the dust band
- * @param dustPresent if dust is present
- * @param gasPresent if gas is present
+ * @constructor Constructor is used make all subsequent dust bands during the splitting process, in which we
+ *              acknowledge dust and gas being removed from part of existing bands.
  *
- * @author zakski
+ * @param innerLimit inner limit of the dust band in AU.
+ * @param outerLimit outer limit of the dust band in AU.
+ * @param dustPresent true if dust is present, false otherwise.
+ * @param gasPresent true if gas is present, false otherwise.
+ *
+ * @author zakski : 06/07/2015.
  */
 protected[accrete] class DustBand(innerLimit: Double, outerLimit: Double, dustPresent: Boolean, gasPresent: Boolean) {
 
+  /**
+   * The edge of the dust band closest to the star.
+   *
+   * @note unit is AU.
+   */
   var innerEdge: Double = innerLimit
 
+  /**
+   * The edge of the dust band furthest to the star.
+   *
+   * @note unit is AU.
+   */
   var outerEdge: Double = outerLimit
 
+  /**
+   * Whether the band has dust remaining.
+   */
   var hasDust: Boolean = dustPresent
 
+  /**
+   * Whether the band has gas remaining.
+   *
+   * @note gas is only removed if a coalescing planet has attained critical mass.
+   */
   var hasGas: Boolean = gasPresent
 
+  /**
+   * Internal LinkedList structure to keep track of dust bands, starting with the closest to the star and
+   * ending with the furthest. If it is not defined then the band is the furthest from the star.
+   */
   var next: Option[DustBand] = None
 
   /**
-   * Initial Constructor during Accretion
+   * Initial Constructor during Accretion. Used solely to form the first band of gas and dust as it is the only
+   * time at which both gas and dust can be assumed to be present.
    *
-   * @param innerLimit inner limit of the dust band
-   * @param outerLimit outer limit of the dust band
+   * @param innerLimit inner limit of the dust band in AU.
+   * @param outerLimit outer limit of the dust band in AU.
    */
   def this(innerLimit: Double, outerLimit: Double) {
     this(innerLimit, outerLimit, true, true)
+  }
+
+  /**
+   * Convenience Method to check if there are more dust bands after it.
+   *
+   * @return true if it is not the furthest dust band, false otherwise.
+   */
+  def hasNext: Boolean = next.isDefined
+
+  /**
+   * Method to supply String representation of the band.
+   *
+   * @return current status of the band in String form.
+   */
+  override def toString: String = {
+    val df: DecimalFormat = new DecimalFormat("#.00000")
+    val build: StringBuilder = new StringBuilder("[band: ")
+    build.append(df.format(innerEdge) + " - ")
+    build.append(df.format(outerEdge) + "AU ")
+    build.append(" dust: " + (if (hasDust) "Y" else "N"))
+    build.append(" gas: " + (if (hasGas) "Y" else "N"))
+    build.append("]")
+    build.toString()
+
   }
 }
--- src/main/scala/com/szadowsz/accrete/base/bodies/ProtoPlanet.scala
+++ src/main/scala/com/szadowsz/accrete/base/bodies/ProtoPlanet.scala
@@ -6,49 +6,109 @@ import com.szadowsz.accrete.base.calc.{AccreteCalc, PlanetaryCalc}
 import com.szadowsz.accrete.util.UnitConverter
 
 /**
- * Class to represent an accreting planetary body. Uses an internal linked-list structure to maintain the
+ * Class to represent an accreting proto planetary body. Uses an internal linked-list structure to maintain the
  * ordering of the protoplanets.
  *
+ * @see Planetismal.java - Ian Burrell (accrete)
+ * @see struct planets_record, line 4 in structs.h - Mat Burdick (accrete)
+ * @see struct planets_record, line 4 in structs.h - Mat Burdick (starform)
+ * @see Protoplanet.java - Carl Burke (starform)
+ *
+ * @constructor Constructor is used make all subsequent dust bands during the splitting process, in which we
+ *              acknowledge dust and gas being removed from part of existing bands.
+ *
+ * @param calc inner limit of the dust band in AU.
+ * @param m outer limit of the dust band in AU.
+ * @param a true if dust is present, false otherwise.
+ * @param e true if gas is present, false otherwise.
+
  * @author Zakski : 25/06/2015.
  */
-
-protected[accrete] class ProtoPlanet(accrete: AccreteCalc, nmass: Double, naxis: Double, necc: Double) extends PlanetaryCalc {
-
-  private val _accrete = accrete
+protected[accrete] class ProtoPlanet(calc: AccreteCalc with PlanetaryCalc, m: Double, a: Double, e: Double) {
 
   /**
-   * mass of Planetismal.
+   * The chosen calculation system for the accretion process.
+   */
+  private val _accrete = calc
+
+  /**
+   * The mas of the planetismal body.
    *
    * @note unit of value is Solar Masses.
    */
-  var mass: Double = nmass
+  var mass: Double = m
 
   /**
-   * semi-major axis of the body's orbit.
+   * The semi-major axis of the body's orbit.
    *
    * @note unit of value is AU.
    */
-  var axis: Double = naxis
+  var axis: Double = a
 
   /**
-   * eccentricity of the body's orbit
+   * The Eccentricity of the body's orbit. How wildly it deviates from the semi-major axis during the course of one
+   * revolution around its planetary body.
    *
-   * @note value between 0.0 and 1.0, range essential for a planet to stay in the system.
+   * @note value between 0.0 (inclusive) and 1.0 (exclusive), this range is essential for the body to stay
+   *       in the system.
    */
-  var ecc: Double = necc
+  var ecc: Double = e
 
+  /**
+   * Internal LinkedList structure to keep track of proto planets, starting with the closest to the star and
+   * ending with the furthest. If it is not defined then the proto planet is the furthest from the star.
+   */
   var next: Option[ProtoPlanet] = None
 
+  /**
+   * The closest to the star that the planet will accrete mass from, given gravitational pull.
+   *
+   * @note unit of value is AU.
+   */
   def innerGravLimit = _accrete.innerGravLimit(perihelion, mass)
 
+  /**
+   * The furthest from the star that the planet will accrete mass from, given gravitational pull.
+   *
+   * @note unit of value is AU.
+   */
   def outerGravLimit = _accrete.outerGravLimit(aphelion, mass)
 
+  /**
+   * The furthest from the star that the planet will be during its orbit..
+   *
+   * @note unit of value is AU.
+   */
+  def aphelion = _accrete.aphelionDistance(axis, ecc)
+
+  /**
+   * The closest to the star that the planet will accrete mass from, given gravitational pull and
+   * cloud particle eccentricity.
+   *
+   * @note unit of value is AU.
+   */
   def innerBandLimit = _accrete.innerBandLimit(_accrete.innerGravLimit(perihelion, mass))
 
+  /**
+   * The closest to the star that the planet will be during its orbit.
+   *
+   * @note unit of value is AU.
+   */
+  def perihelion = _accrete.perihelionDistance(axis, ecc)
+
+  /**
+   * The furthest from the star that the planet will accrete mass from, given gravitational pull and
+   * cloud particle eccentricity.
+   *
+   * @note unit of value is AU.
+   */
   def outerBandLimit = _accrete.outerBandLimit(_accrete.outerGravLimit(aphelion, mass))
 
-  def aphelion = aphelionDistance(axis, ecc)
-
+  /**
+   * Method to supply String representation of the proto planet.
+   *
+   * @return current status of the  proto planet in String form.
+   */
   override def toString: String = {
     var df: DecimalFormat = new DecimalFormat("#.00000")
     val build: StringBuilder = new StringBuilder("[axis: ")
@@ -65,9 +125,15 @@ protected[accrete] class ProtoPlanet(accrete: AccreteCalc, nmass: Double, naxis:
     build.toString()
   }
 
+  /**
+   * Whether the planet has surpassed its critical mass limit and become a gas giant.
+   */
   def isGas = mass >= criticalMass
 
+  /**
+   * the limit at which a proto planet has gained enough mass to accrete gas as well as dust
+   *
+   * @note unit of value is Solar Masses.
+   */
   def criticalMass = _accrete.criticalMass(perihelion)
-
-  def perihelion = perihelionDistance(axis, ecc)
 }
