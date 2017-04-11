6513ba22d53d188c24c1ecf6943af7182025677f
zakski <szadowsz@tcd.ie>
2017-04-11 15:43:15 +0100

    author profile experimentation

---
+++ modules/recreation/src/main/scala/com/szadowsz/starform/profile/AlgoProfile.scala
@@ -0,0 +1,15 @@
+package com.szadowsz.starform.profile
+
+import com.szadowsz.starform.accrete.constants.AccreteConstants
+import com.szadowsz.starform.rand.RandGenTrait
+
+/**
+  * Created on 11/04/2017.
+  */
+trait AlgoProfile {
+
+  val rand : RandGenTrait
+
+  val accConsts : AccreteConstants
+
+}
---
+++ modules/recreation/src/main/scala/com/szadowsz/starform/profile/FKeris2Profile.scala
@@ -0,0 +1,13 @@
+package com.szadowsz.starform.profile
+import com.szadowsz.starform.accrete.constants.{AccreteConstants, FKeris2Constants}
+import com.szadowsz.starform.rand.{JDKRandGen, RandGenTrait}
+
+/**
+  * Created on 11/04/2017.
+  */
+class FKeris2Profile extends AlgoProfile{
+
+  override val rand: RandGenTrait = new JDKRandGen() // TODO check if this aligns to the c/c++ variant
+
+  override val accConsts: AccreteConstants = new FKeris2Constants()
+}
